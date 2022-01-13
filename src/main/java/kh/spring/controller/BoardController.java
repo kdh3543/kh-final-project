170aef9d7d831b0e299176dbee53a8e11d7fef9a
seanparkhere <pshazx920116@gmail.com>
2022-01-13 17:01:00 +0900

    Created boardDetail.jsp and writeForm.jsp / Modified boardDetail.jsp and boardList.jsp

--- src/main/java/kh/spring/controller/BoardController.java
+++ src/main/java/kh/spring/controller/BoardController.java
@@ -1,6 +1,8 @@
 package kh.spring.controller;
 
+import java.io.File;
 import java.util.List;
+import java.util.UUID;
 
 import javax.servlet.http.HttpSession;
 
@@ -8,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 import kh.spring.dto.BoardDTO;
 import kh.spring.service.BoardService;
@@ -29,6 +32,26 @@ public class BoardController {
 		List<BoardDTO> list = service.selectAll();
 		model.addAttribute("list", list);
 		return "board/boardList";
+	}
+	
+	@RequestMapping("writeForm")
+	public String writeForm() {
+		System.out.println("writeForm 로 들어온 요청은 이 메서드를 실행합니다.");
+
+		return "board/writeForm";
+	}
+
+	@RequestMapping("toDetail")
+	public String toDetail(int seq, Model model) {
+		System.out.println("toDetail 로 들어온 요청은 이 메서드를 실행합니다.");
+
+		BoardDTO dto = service.selectBySeq(seq);
+		model.addAttribute("dto", dto);
+		
+//		List<FilesDTO> list = fservice.selectBySeq(dto.getSeq());
+//		model.addAttribute("list", list);
+		
+		return "board/boardDetail";
 	}
 
 }
