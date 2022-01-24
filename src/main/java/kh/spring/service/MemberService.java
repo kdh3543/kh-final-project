2be28cf623b577aa3a57b7c7ed65ffcf7cfa9a11
KwakSeoHo <wishstar1998@naver.com>
2022-01-24 18:43:46 +0900

    confilct error complete

--- src/main/java/kh/spring/service/MemberService.java
+++ src/main/java/kh/spring/service/MemberService.java
@@ -20,96 +20,95 @@ import kh.spring.utils.EncryptionUtils;
 
 @Service
 public class MemberService {
-	
-	@Autowired
-	public MemberDAO mdao;
-	
-	@Autowired
-	private HttpSession session;
-	//id중복체크
-	public int idCheck(String id) throws Exception{
-		return mdao.idCheck(id);
-	}
-	//회원가입 정보 입력
-	public int insert(MemberDTO dto,MultipartFile file) throws Exception{
-		String realPath = session.getServletContext().getRealPath("signup");
-		File realPathFile = new File(realPath);
-		if(!realPathFile.exists()) {
-			realPathFile.mkdir();
-		}
-		String oriName = file.getOriginalFilename();
-		String sysName = UUID.randomUUID()+"_"+ oriName;
-		
-		file.transferTo(new File(realPath+"/"+sysName));
-		dto.setProfile_image(sysName);
-	
-		//비밀번호 암호화 작업
-		String encPw = EncryptionUtils.getSHA512(dto.getPw());
-		dto.setPw(encPw);
-		return mdao.insert(dto);
-	}
-	//kakao회원 정보 입력
-	public int kakaoinsert(MemberDTO dto)throws Exception{
-		return mdao.kakaoinsert(dto);
-	}
-	//이미 가입한 카카오 회원
-	public MemberDTO selectBykakaoemail(String kakaoemail) {
-		return mdao.selectBykakaoemail(kakaoemail);
-	}
-	//아이디로 찾기
-	public MemberDTO select(String id){
-		return mdao.select(id);
-	}
-	//아이디 삭제
-	public int delete(String id) {
-		return mdao.delete(id);
-	}
-	//로그인하기
-	public int login(String logid, String logpw) {
-		int result =  mdao.login(logid, logpw);
-		if(result!=1) {
-			result = 0;
-			return result;
-		}
-		return result;
-	}
-	//회원 정보 수정
-	public int modify(MemberDTO dto) {
-		return mdao.modify(dto);
-	}
-	//가입 날짜
-	public int signDate(String id) {
-		return mdao.signDate(id);
-	}
-	//카카오 기존 회원 여부 확인
-	public int selectBykakao(String kakaoemail) {
-		return mdao.selectBykakao(kakaoemail);
-	}
-	//ID 찾기
-	public MemberDTO selectID(String email,String phone) {
-		return mdao.selectID(email,phone);
-	}
-	//id존재여부확인
-	public int selectIDexist(String email,String phone) {
-		return mdao.selectIDexist(email, phone);
-	}
-	//아이디 이메일 일치 여부
-	public int AccountExist(String inputID,String inputEmail) {
-		System.out.println(inputID+"Service");
-		System.out.println(inputEmail+"Service");
-		int result = mdao.AccountExist(inputID, inputEmail);
-		 if(result<1) {
-			result=0;
-			return result;
-		}
-		return result;
-	}
-	//비밀번호 변경
-	public int updatePw(String pw, String id) {
-		String encpw = EncryptionUtils.getSHA512(pw);
-		return mdao.updatePw(encpw,id);
-	}
-	
+   
+   @Autowired
+   public MemberDAO mdao;
+   
+   @Autowired
+   private HttpSession session;
+   //id중복체크
+   public int idCheck(String id) throws Exception{
+      return mdao.idCheck(id);
+   }
+   //회원가입 정보 입력
+   public int insert(MemberDTO dto,MultipartFile file) throws Exception{
+      String realPath = session.getServletContext().getRealPath("signup");
+      File realPathFile = new File(realPath);
+      if(!realPathFile.exists()) {
+         realPathFile.mkdir();
+      }
+      String oriName = file.getOriginalFilename();
+      String sysName = UUID.randomUUID()+"_"+ oriName;
+      
+      file.transferTo(new File(realPath+"/"+sysName));
+      dto.setProfile_image(sysName);
+   
+      //비밀번호 암호화 작업
+      String encPw = EncryptionUtils.getSHA512(dto.getPw());
+      dto.setPw(encPw);
+      return mdao.insert(dto);
+   }
+   //kakao회원 정보 입력
+   public int kakaoinsert(MemberDTO dto)throws Exception{
+      return mdao.kakaoinsert(dto);
+   }
+   //이미 가입한 카카오 회원
+   public MemberDTO selectBykakaoemail(String kakaoemail) {
+      return mdao.selectBykakaoemail(kakaoemail);
+   }
+   //아이디로 찾기
+   public MemberDTO select(String id){
+      return mdao.select(id);
+   }
+   //아이디 삭제
+   public int delete(String id) {
+      return mdao.delete(id);
+   }
+   //로그인하기
+   public int login(String logid, String logpw) {
+      int result =  mdao.login(logid, logpw);
+      if(result!=1) {
+         result = 0;
+         return result;
+      }
+      return result;
+   }
+   //회원 정보 수정
+   public int modify(MemberDTO dto) {
+      return mdao.modify(dto);
+   }
+   //가입 날짜
+   public int signDate(String id) {
+      return mdao.signDate(id);
+   }
+   //카카오 기존 회원 여부 확인
+   public int selectBykakao(String kakaoemail) {
+      return mdao.selectBykakao(kakaoemail);
+   }
+   //ID 찾기
+   public MemberDTO selectID(String email,String phone) {
+      return mdao.selectID(email,phone);
+   }
+   //id존재여부확인
+   public int selectIDexist(String email,String phone) {
+      return mdao.selectIDexist(email, phone);
+   }
+   //아이디 이메일 일치 여부
+   public int AccountExist(String inputID,String inputEmail) {
+      System.out.println(inputID+"Service");
+      System.out.println(inputEmail+"Service");
+      int result = mdao.AccountExist(inputID, inputEmail);
+       if(result<1) {
+         result=0;
+         return result;
+      }
+      return result;
+   }
+   //비밀번호 변경
+   public int updatePw(String pw, String id) {
+      String encpw = EncryptionUtils.getSHA512(pw);
+      return mdao.updatePw(encpw,id);
+   }
+   
 
 }
-
--- src/main/webapp/WEB-INF/views/member/signIn.jsp
+++ src/main/webapp/WEB-INF/views/member/signIn.jsp
@@ -3,6 +3,7 @@
 
 <head>
     <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -86,10 +87,8 @@
                 </form>
                 <div class="buttons">
                 
-
                </div>
                
-
         </div>
     </main>
     <footer>
@@ -100,7 +99,6 @@
     </footer>
     
     <script>
-
         $("#findMyInfo").on("click",function(){
             location.href="findInfo";
         });
